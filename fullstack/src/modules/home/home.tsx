7096833289e0f8e6f4c1ab05360c50bf08af0e6c
Abu Huraryah <sahilakhtar5059@gmail.com>
2024-05-24 18:08:05 +0530

    Created Banner

--- fullstack/src/modules/home/home.tsx
+++ fullstack/src/modules/home/home.tsx
@@ -1,11 +1,13 @@
 //imports
 import Header from "@/shared/widgets/header/header";
+import Banner from "./features/banner";
 
 const Home = () => {
   return (
     <div>
         {/* watch- 24:45 to 25:15 */}
         <Header />
+        <Banner />
     </div>
   )
 }
